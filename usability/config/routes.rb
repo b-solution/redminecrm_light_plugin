0d4361e135a08aa2ac60b8fe91dec9a65fffb926
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-09-30 22:15:14 +0100

    commit

---
+++ usability/config/routes.rb
@@ -0,0 +1,11 @@
+match '404', to: 'us_errors#error_404', via: [:get]
+match '500', to: 'us_errors#error_500', via: [:get]
+
+get 'users/:id/edit_usability_preferences', controller: 'users', action: 'edit_usability_preferences', id: /\d+/
+get 'users/:id/show_user_details', controller: 'users', action: 'show_user_details', id: /\d+/, as: "show_user_details"
+post 'custom_fields/move_postion', controller: 'custom_fields', action: 'move_to_position'
+
+get 'easy_perplex', controller: :easy_perplex, action: :easy_perplex
+get 'easy_perplex_actions(/:user_id)', controller: :easy_perplex, action: :easy_perplex_actions, user_id: /\d+/
+
+get 'attachments/download/all/:id', controller: :attachments, action: :download_all
